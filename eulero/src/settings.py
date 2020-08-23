8f704a37f625507eaf872c7cc4daa813495fe175
Luca8991 <luca.berte98@hotmail.it>
2020-08-23 14:31:39 +0200

    settings updated and fixed

--- eulero/src/settings.py
+++ eulero/src/settings.py
@@ -1,69 +1,68 @@
+import asyncio
+
 class Settings:
-	def __init__(self, app):
-		self.app=app
-		self.contrast=0
-		self.rotation=0
+    def __init__(self, omniacls):
+        self.omniacls=omniacls
+        self.contrast=0
+        self.rotation=0
+        self.i = 0
+        self.changed=True
+        self.close_app=False
+    
+    def clickCallback(self, clickedBtn):
+        if(clickedBtn == "SELECT"):
+            if(self.i==2):
+                self.close_app=True
+            elif(self.i==1):
+                self.rotation = not self.rotation
+                self.omniacls.setRotation(self.rotation)
+                self.changed=True
+            elif(self.i==0):
+                self.contrast = not self.contrast
+                self.omniacls.setContrast(self.contrast)
+                self.changed=True
+        
+        elif(clickedBtn == "DOWN"):
+            if(self.i==2):
+                self.i=0
+            else:
+                self.i+=1
 
-	def run(self):
-		change=True
-		close_app=False
-		datau_old=0
-		datad_old=0
-		datas_old=0
-		next_app=False
-		i=0
-		while((not close_app) and self.app.isAlive()):
-			if (change):
-				change=False
-				self.app.newImg()
-				self.app.setText((35,0), "SETTINGS ", 255,self.app.getFonts()[0])
-				self.app.setText((10,10),"CONTRAST ", 255,self.app.getFonts()[0])
-				self.app.setText((10,20),"ROTATION ", 255,self.app.getFonts()[0])
-				self.app.setText((10,30),"MENU ", 255,self.app.getFonts()[0])
-				self.app.setText((1,10+i*10),">", 255,self.app.getFonts()[0])
-				data=self.app.sendImg_and_recvData()
-			else:
-				data=self.app.recvData()
+            self.changed=True
+            
+        elif(clickedBtn == "UP"):
+            if(self.i==0):
+                self.i=2
+            else:
+                self.i-=1
+            
+            self.changed=True
 
-			#print(data)	
-			if (data['UP']!=datau_old):
-				datau_old=data['UP']
-				if(datau_old):
-					if(i==0):
-						i=2
-					else:
-						i-=1
+    async def run(self):
 
-				change=True
-			elif (data['DOWN']!=datad_old):
-				datad_old=data['DOWN']
-				if(datad_old):
-					if(i==2):
-						i=0
-					else:
-						i+=1
+        self.omniacls.setButtonCallback(self.clickCallback)
 
-					change=True
+        self.changed=True
+        self.close_app=False
+        
+        while((not self.close_app) and self.omniacls.isAlive()):
+            if (self.changed):
+                self.changed=False
+                self.omniacls.newImg()
 
-			elif(data['SELECT']!=datas_old):
-				datas_old=data['SELECT']
-				if(datas_old):
-					if(i==2):
-						next_app=True
-					elif(i==1):
-						self.rotation = not self.rotation
-						self.app.setRotation(self.rotation)
-						change=True
-					elif(i==0):
-						self.contrast = not self.contrast
-						self.app.setContrast(self.contrast)
-						change=True
+                self.omniacls.setText((35,0), "SETTINGS ", 255,self.omniacls.getFonts()[0])
+                self.omniacls.setText((10,10),"CONTRAST ", 255,self.omniacls.getFonts()[0])
+                self.omniacls.setText((10,20),"ROTATION ", 255,self.omniacls.getFonts()[0])
+                self.omniacls.setText((10,30),"MENU ", 255,self.omniacls.getFonts()[0])
+                self.omniacls.setText((1,10+self.i*10),">", 255,self.omniacls.getFonts()[0])
+                
+                self.omniacls.sendImg()
+            
+            await asyncio.sleep(0.1)
 
-			if (next_app and data['SELECT']==0):
-				next_app=False
-				close_app = True
+        self.close_app = False
 
-		return -1
+        return -1
 
 
 
